c7da645198492704816918edeeec804789d46634
nsazhi <n190275@inbox.ru>
2025-01-03 13:20:02 +0400

    add models

--- mff_app/catalog/models.py
+++ mff_app/catalog/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Film(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100)
+    release = models.IntegerField()
+    country = models.CharField(max_length=100)
+    genre = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    actors = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    img_url = models.CharField(max_length=200)
+    is_viewed = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
