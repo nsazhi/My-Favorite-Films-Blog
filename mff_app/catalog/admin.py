f83fea97a41a310d96f2265bbed9c61505c8c599
nsazhi <n190275@inbox.ru>
2025-01-03 15:06:04 +0400

    add postgresql, migrations, add admin-classes, add created_at in model

--- mff_app/catalog/admin.py
+++ mff_app/catalog/admin.py
@@ -1,23 +1,29 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Category)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
 
 @admin.register(Film)
 class FilmAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category')
-    search_fields = ('title', )
-    list_filter = ('category', 'genre', 'country')
+    list_display = ('title', 'category', 'country')
+    search_fields = ('title', 'country')
+    list_filter = ('category', 'genre')
     list_per_page = 20
     fieldsets = (
         ('Основная информация', {
-            'fields': ('category', 'title', 'release', 'country', 'genre')
+            'fields': ('category', 'title', 'slug', 'release', 'country', 'genre')
         }),
         ('Расширенная информация', {
             'fields': ('director', 'actors', 'description')
         }),
         ('Ссылка на изображение', {
-            'fields': ('img_url', )
+            'fields': ('img_url',)
         }),
     )
-    readonly_fields = ('slug', 'is_viewed')
+    prepopulated_fields = {'slug': ('title',)}
+    readonly_fields = ('created_at', 'is_viewed')
--- mff_app/catalog/models.py
+++ mff_app/catalog/models.py
@@ -1,17 +1,16 @@
 from django.db import models
 
-
 class Category(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-    slug = models.SlugField(name)
+    name = models.CharField(max_length=50, unique=True, db_index=True)
+    slug = models.SlugField(max_length=100, unique=True, db_index=True)
 
     def __str__(self):
         return self.name
 
 
 class Film(models.Model):
-    title = models.CharField(max_length=100, unique=True)
-    slug = models.SlugField(title)
+    title = models.CharField(max_length=100, unique=True, db_index=True)
+    slug = models.SlugField(max_length=200, unique=True, db_index=True)
     release = models.IntegerField(null=False)
     country = models.CharField(max_length=100, null=False)
     genre = models.CharField(max_length=100, null=False)
@@ -21,6 +20,7 @@ class Film(models.Model):
     img_url = models.CharField(max_length=200)
     is_viewed = models.BooleanField(default=False)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    created_at = models.DateField(auto_now_add=True)
 
     def __str__(self):
         return self.title
